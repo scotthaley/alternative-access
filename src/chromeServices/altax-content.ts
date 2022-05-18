26cd4bc14d84e839aad72db49c482dcba926cebc
Scott Haley <williamscotthaley@gmail.com>
2022-05-17 23:18:15 -0500

    Fix issue with content script being injected in popup, initiate keyboard page in iframe

--- src/chromeServices/altax-content.ts
+++ src/chromeServices/altax-content.ts
@@ -3,6 +3,7 @@ import {DOMMessage} from '../types';
 import {FocusEngine} from './focus-engine';
 import {ApplyProfile, IAltAxProfile, ProfileSectionType} from './profile-applicator';
 
+
 const YouTubeProfile: IAltAxProfile = {
   sections: [
     {
@@ -38,94 +39,105 @@ const YouTubeProfile: IAltAxProfile = {
   ]
 };
 
-// const SimulateKeystroke = (keyCode: number) => {
-//   document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': keyCode}))
-// }
-
 let video: HTMLVideoElement;
 
-const engine = new SequenceEngine({
-  switch1: 'ArrowDown'
-});
+const init = () => {
+  const engine = new SequenceEngine({
+    switch1: 'ArrowDown'
+  });
 
-engine.RegisterCallback(SequenceType.Switch1Press, () => {
-  switch (focusEngine.currentMode.type) {
-    case ProfileSectionType.CYCLE:
-      focusEngine.CycleNext();
-      break;
-    case ProfileSectionType.VIDEO:
-      // SimulateKeystroke(32);\
-      // TODO: has to be a better way to make this play
-      // Does each profile have methods to be used?
-      video.paused ? video.play() : video.pause();
-      break;
+  engine.RegisterCallback(SequenceType.Switch1Press, () => {
+    switch (focusEngine.currentMode.type) {
+      case ProfileSectionType.CYCLE:
+        focusEngine.CycleNext();
+        break;
+      case ProfileSectionType.SEARCH:
+        const f = document.createElement('iframe');
+        f.src = chrome.runtime.getURL('index.html') + '/#/kb';
+        f.style.zIndex = '99999';
+        f.style.position = 'absolute';
+        f.style.top = '0';
+        f.style.left = '0';
+        f.width = '800px';
+        f.height = '600px';
+        document.body.appendChild(f);
+        break;
+      case ProfileSectionType.VIDEO:
+        // SimulateKeystroke(32);\
+        // TODO: has to be a better way to make this play
+        // Does each profile have methods to be used?
+        video.paused ? video.play() : video.pause();
+        break;
+    }
+  });
+
+  engine.RegisterCallback(SequenceType.Switch1LongPress, () => {
+    switch (focusEngine.currentMode.type) {
+      case ProfileSectionType.CYCLE:
+        focusEngine.CyclePrevious();
+        break;
+      case ProfileSectionType.VIDEO:
+        // SimulateKeystroke(32);
+
+        break;
+    }
+  });
+
+  engine.RegisterCallback(SequenceType.Switch1DoublePress, () => {
+    switch (focusEngine.currentMode.type) {
+      case ProfileSectionType.CYCLE:
+        focusEngine.CycleEnter();
+        break;
+      case ProfileSectionType.VIDEO:
+        video.volume = video.volume - .05;
+        // SimulateKeystroke(32);
+        break;
+    }
+  });
+
+  engine.RegisterCallback(SequenceType.Switch1TriplePress, () => {
+    focusEngine.CycleMode();
+  });
+
+  const focusEngine = new FocusEngine(YouTubeProfile);
+
+  const enableEngine = () => {
+    document.body.append(focusEngine.focusSteal);
+    ApplyProfile(YouTubeProfile);
+    focusEngine.FocusMode();
+    engine.enabled = true;
+    document.body.append(focusEngine.modeDisplayElement);
   }
-});
 
-engine.RegisterCallback(SequenceType.Switch1LongPress, () => {
-  switch (focusEngine.currentMode.type) {
-    case ProfileSectionType.CYCLE:
-      focusEngine.CyclePrevious();
-      break;
-    case ProfileSectionType.VIDEO:
-      // SimulateKeystroke(32);
-     
-      break;
+  const disableEngine = () => {
+    engine.enabled = false;
+    document.body.removeChild(focusEngine.modeDisplayElement);
   }
-});
 
-engine.RegisterCallback(SequenceType.Switch1DoublePress, () => {
-  switch (focusEngine.currentMode.type) {
-    case ProfileSectionType.CYCLE:
-      focusEngine.CycleEnter();
-      break;
-    case ProfileSectionType.VIDEO:
-      video.volume = video.volume - .05;
-      // SimulateKeystroke(32);
-      break;
-  }
-});
-
-engine.RegisterCallback(SequenceType.Switch1TriplePress, () => {
-  focusEngine.CycleMode();
-});
-
-const focusEngine = new FocusEngine(YouTubeProfile);
-
-const enableEngine = () => {
-  document.body.append(focusEngine.focusSteal);
-  ApplyProfile(YouTubeProfile);
-  focusEngine.FocusMode();
-  engine.enabled = true;
-  document.body.append(focusEngine.modeDisplayElement);
-}
-
-const disableEngine = () => {
-  engine.enabled = false;
-  document.body.removeChild(focusEngine.modeDisplayElement);
-}
-
-chrome.runtime.onMessage.addListener((msg: DOMMessage, sender, sendResponse) => {
-  switch (msg.type) {
-    case 'ENABLE':
-      enableEngine();
-      sendResponse(true);
-      chrome.runtime.sendMessage({type: 'ENABLE'} as DOMMessage)
-      break;
-    case 'DISABLE':
-      disableEngine();
-      sendResponse(false);
-      chrome.runtime.sendMessage({type: 'DISABLE'} as DOMMessage)
-      break;
-    case 'STATUS':
-      sendResponse(engine.enabled);
-      break;
-  }
-})
-
-setTimeout(() => {
-  video = document.getElementsByTagName('video')[0];
-  chrome.runtime.sendMessage({type: 'STATUS'} as DOMMessage, response => {
-    response ? enableEngine() : disableEngine();
+  chrome.runtime.onMessage.addListener((msg: DOMMessage, sender, sendResponse) => {
+    switch (msg.type) {
+      case 'ENABLE':
+        enableEngine();
+        sendResponse(true);
+        chrome.runtime.sendMessage({type: 'ENABLE'} as DOMMessage)
+        break;
+      case 'DISABLE':
+        disableEngine();
+        sendResponse(false);
+        chrome.runtime.sendMessage({type: 'DISABLE'} as DOMMessage)
+        break;
+      case 'STATUS':
+        sendResponse(engine.enabled);
+        break;
+    }
   })
-}, 1000)
+
+  setTimeout(() => {
+    video = document.getElementsByTagName('video')[0];
+    chrome.runtime.sendMessage({type: 'STATUS'} as DOMMessage, response => {
+      response ? enableEngine() : disableEngine();
+    })
+  }, 1000)
+}
+
+if (window.location.href.indexOf('chrome-extension') === -1) init();
---
+++ src/pages/Keyboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Keyboard: React.FC = () => {
+  return (
+    <h1>Keyboard</h1>
+  )
+}
+
+export default Keyboard;
